cc14a174a10d9aade530e9d85c1166422aa14e5d
MarcinMichna <marcinxmichna@gmail.com>
2020-03-01 20:00:46 +0100

    Initial commit

---
+++ src/main/java/pl/michnamarcin/bookrate/BookrateApplication.java
@@ -0,0 +1,15 @@
+package pl.michnamarcin.bookrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookrateApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BookrateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/michnamarcin/bookrate/BookrateApplicationTests.java
@@ -0,0 +1,15 @@
+package pl.michnamarcin.bookrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookrateApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
